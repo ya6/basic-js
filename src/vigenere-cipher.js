4c7be8e6edabe12a294f66427d63ba57655617a9
YA <vector_cp@mail.ru>
2020-10-03 22:48:58 +0300

    feat: add decription

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -44,9 +44,42 @@ class VigenereCipheringMachine {
     return rez;
 
   }
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(str, key) {
+   
+    let _str = str.toUpperCase();
+    let _key = key.toUpperCase();
+
+
+    let __key = '';
+    let i = 0;
+    //  console.log(_key.length < _str.length)
+    if (_key.length < _str.length) {
+      let rep = Math.ceil(_str.length / _key.length);
+      _key = _key.repeat(rep);
+
+      for (let index = 0; index < _str.length; index++) {
+        if (_str.charCodeAt(index) >= 65 && _str.charCodeAt(index) <= 90) {
+          __key += _key[i++];
+
+        } else __key += _str[index];
+
+      }
+    }
+    //   console.log('__key ' + __key);
+    let rez = '';
+    let char = 0;
+
+    for (let index = 0; index < str.length; index++) {
+      if (_str.charCodeAt(index) >= 65 && _str.charCodeAt(index) <= 90) {
+        char = (_str.charCodeAt(index)) - (__key.charCodeAt(index))+65;
+        if (char < 65) char += 26;
+        char = String.fromCharCode(char);
+      } else char = _str[index];
+
+      rez += char;
+    }
+    rez = (this.flag) ? rez : rez.split("").reverse().join("");
+    return rez;
   }
 }
 
